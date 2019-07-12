6aa66af4ad6a3916ce948d320a9d10dbd38cc18e
Administrator <1179911433@qq.com>
2019-07-12 23:10:10 +0800

    init

---
+++ test/src/main/java/name/cuimy/git/Test.java
@@ -0,0 +1,11 @@
+package name.cuimy.git;
+
+/**
+ * Created by Administrator on 2019/7/12.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("2222");
+    }
+}
