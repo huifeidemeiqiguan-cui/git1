c1234cdec5df7638d776976de69d9b6ae17f4957
Administrator <1179911433@qq.com>
2019-07-12 23:15:57 +0800

    update

--- test/src/main/java/name/cuimy/git/Test.java
+++ test/src/main/java/name/cuimy/git/Test.java
@@ -5,7 +5,7 @@ package name.cuimy.git;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("1111");
+        System.out.println("222");
         System.out.println("333");
         System.out.println("45");
     }
